fb24217c74902f6eb1fe1aa2a5e40af281983b35
Denzil Gumbo <gdenzilg@gmail.com>
2018-02-05 15:08:03 +0200

    Lesson 4

--- src/main/java/com/denzilgumbo/codility/lessons/_04_counting_elements/PermCheck.java
+++ src/main/java/com/denzilgumbo/codility/lessons/_04_counting_elements/PermCheck.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.denzilgumbo.codility.lessons._03_time_complexity;
- 
+package com.denzilgumbo.codility.lessons._04_counting_elements ;
+  
 import java.util.Arrays;
 
 
@@ -12,29 +12,31 @@ import java.util.Arrays;
  *
  * @author denzil
  */
-public class PermMissingElem {
+public class PermCheck {
         public static void main(String[] args) {
-        PermMissingElem ps = new PermMissingElem(); 
+        PermCheck ps = new PermCheck(); 
         
-        int[] A = new int[]{2, 3, 1, 5};
+        //int[] A = new int[]{2, 3, 1, 5};
+        //int[] A = new int[]{4, 1, 3};
+        int[] A = new int[]{4, 1, 3, 2};
 
-        System.out.println("TapeEquilibrium : " + ps.solution(A));
+        System.out.println("PermCheck : " + ps.solution(A));
     }   
         
     public int solution(int[] A) {
         // write your code in Java SE 8
         
         int iLength = A.length; 
-        if (iLength==0) return 1;
+        if (iLength==0) return 0;
         
         Arrays.sort(A); 
         
-        if (A[0] == 2) return 1; 
+        if (A[0] >= 2) return 0; 
         
         for(int i = 0 ; i < iLength - 1; i++){
-            if (A[i] + 1 != A[i + 1]) return A[i] + 1 ;
+            if (A[i] + 1 != A[i + 1]) return 0 ;
         }
         
-        return A[iLength-1] + 1;
+        return  1;
     }
 }
